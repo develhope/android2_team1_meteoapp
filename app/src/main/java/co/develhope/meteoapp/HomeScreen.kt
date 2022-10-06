c7228e8fc2a672ee50782b216099f0c1f3a960e3
Edoardo Di Chiara <dichiaraedoardo@gmail.com>
2022-10-06 15:54:56 +0200

    Add text view on Home Screen Update gradle with sdk 33 recycler view dependencies implementation fix dimens.xml

--- app/src/main/java/co/develhope/meteoapp/HomeScreen.kt
+++ app/src/main/java/co/develhope/meteoapp/HomeScreen.kt
@@ -5,13 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import co.develhope.meteoapp.databinding.FragmentHomeScreenBinding
 
 class HomeScreen : Fragment() {
+    private var bindingHomeScreen: FragmentHomeScreenBinding? = null
+    private val binding get() = bindingHomeScreen!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
-        return inflater.inflate(R.layout.fragment_home_screen, container, false)
+        bindingHomeScreen = FragmentHomeScreenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
