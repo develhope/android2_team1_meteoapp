661d191e16adae6cd4cd2d30ecb9ea2a7ee8b3d5
Giulio <giulioaterno@gmail.com>
2022-10-10 17:11:51 +0200

    - Created TodayScreen with recycler view that shows infos about hour, temperature and rain percentage.  -Fixed some colours. - Created TodayScreen Adapter.

---
+++ app/src/main/java/co/develhope/meteoapp/TodayScreenAdapter.kt
@@ -0,0 +1,50 @@
+package co.develhope.meteoapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class TodayScreenViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    val todayHour: TextView
+    val todayIcon: ImageView
+    val todayTemperature: TextView
+    val todayPercentage: TextView
+
+    init {
+
+        todayHour = view.findViewById(R.id.todayHour)
+        todayIcon = view.findViewById(R.id.todayIcon)
+        todayTemperature = view.findViewById(R.id.todayTemperature)
+        todayPercentage = view.findViewById(R.id.todayPercentage)
+
+    }
+
+}
+
+
+class TodayScreenAdapter(val todayList: List<TodayCardInfo>) :
+    RecyclerView.Adapter<TodayScreenViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodayScreenViewHolder {
+        val todayScreenView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.today_recycler_view_item, parent, false)
+        return TodayScreenViewHolder(todayScreenView)
+    }
+
+    override fun onBindViewHolder(holder: TodayScreenViewHolder, position: Int) {
+
+        holder.todayHour.text = todayList[position].date.hour.toString()
+        holder.todayTemperature.text = todayList[position].temperature.toString()
+        holder.todayPercentage.text = todayList[position].rainfall.toString()
+
+    }
+
+    override fun getItemCount(): Int {
+        return todayList.size
+    }
+
+
+}
