73dc4f56d716e71d7e1ca061a303dab3a157e33f
Edoardo Di Chiara <dichiaraedoardo@gmail.com>
2022-11-07 14:44:56 +0100

    Rename function to setupUi

--- app/src/main/java/co/develhope/meteoapp/HomeScreen.kt
+++ app/src/main/java/co/develhope/meteoapp/HomeScreen.kt
@@ -6,14 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView.Adapter
 import co.develhope.meteoapp.databinding.FragmentHomeScreenBinding
-import org.threeten.bp.OffsetDateTime
 
 class HomeScreen : Fragment() {
     private var bindingHomeScreen: FragmentHomeScreenBinding? = null
     private val binding get() = bindingHomeScreen!!
-    private val adapterList by lazy { HomeScreenAdapter() }
+    private val homeScreenAdapter by lazy { HomeScreenAdapter() }
 
 
     override fun onCreateView(
@@ -27,15 +25,15 @@ class HomeScreen : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        adapterInstanceHomeScreen()
+        setupUi()
     }
 
-    fun adapterInstanceHomeScreen(){
+    private fun setupUi(){
         val itemList = ForecastInfoObject.getHomeScreenItem()
-        adapterList.updateList(itemList)
+        homeScreenAdapter.updateList(itemList)
         binding.weatherHomeScreenList.apply {
             layoutManager = LinearLayoutManager(this@HomeScreen.context, LinearLayoutManager.VERTICAL, false)
-            adapter = adapterList
+            adapter = homeScreenAdapter
         }
     }
 }
