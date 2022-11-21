c49e7b7f4ecbff3a7109b86b69f6949ee523b995
Giulio <giulioaterno@gmail.com>
2022-11-21 17:40:46 +0100

    Added CardView for TodayFragment (worked on UI Only)

--- app/src/main/java/co/develhope/meteoapp/TodayScreenAdapter.kt
+++ app/src/main/java/co/develhope/meteoapp/TodayScreenAdapter.kt
@@ -66,6 +66,11 @@ class TodayScreenAdapter(val list: List<TodayScreenItem>) :
                 title.date.monthValue,
                 title.date.year
             )
+            titleTodayScreenItemBinding.TodayLocation.text = itemView.context.getString(
+                R.string.tv_home_title,
+                title.city,
+                title.region
+            )
         }
 
     }
