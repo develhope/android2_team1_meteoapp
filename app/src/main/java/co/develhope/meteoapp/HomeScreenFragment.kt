ca1077e27369809529cd6af1dc157893c63ccaf4
Edoardo Di Chiara <dichiaraedoardo@gmail.com>
2022-11-28 11:09:55 +0100

    Sorting list for hour

--- app/src/main/java/co/develhope/meteoapp/HomeScreenFragment.kt
+++ app/src/main/java/co/develhope/meteoapp/HomeScreenFragment.kt
@@ -46,7 +46,6 @@ class HomeScreenFragment : Fragment() {
     }
 
     private fun setupUi(forecastList: List<CardInfo>) {
-        //TODO ordinare la lista per giorno
         forecastList.sortedBy { it.date }
         val itemsToShow: List<HomeScreenItem> = getItemsToShow(forecastList.toMutableList())
         val homeScreenAdapter: HomeScreenAdapter = HomeScreenAdapter(itemsToShow,
