fdc19ca4210e7bf35a8818a2ee3315368f4d8412
Giulio <giulioaterno@gmail.com>
2022-12-07 20:31:00 +0100

    à

--- app/src/main/java/co/develhope/meteoapp/ErrorPageFragment.kt
+++ app/src/main/java/co/develhope/meteoapp/ErrorPageFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 
 class ErrorPageFragment : Fragment() {
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
